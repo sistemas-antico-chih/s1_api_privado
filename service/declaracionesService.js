b60601bfaedbdec835d1b49bbf65472a1893fd31
= <=>
2022-10-20 13:00:27 -0600

    conflicto de interes :false

--- service/declaracionesService.js
+++ service/declaracionesService.js
@@ -880,6 +880,7 @@ async function post_declaraciones(body) {
               }
             },
             "interes": {
+              actualizacionConflictoInteres: false,
               participacion: {
                 ninguno: ningunoParticipacion,
                 participacion: participacion1
