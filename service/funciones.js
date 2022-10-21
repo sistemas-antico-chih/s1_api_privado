05039e0f349bea4246fc0720c0ecdc31c04f3d5c
= <=>
2022-10-21 15:25:23 -0600

    probar bienesMuebles tercero null

--- service/funciones.js
+++ service/funciones.js
@@ -366,55 +366,52 @@ function bienesMuebles(bienMueble) {
   bienMueble.forEach((n) => {
     n.fechaAdquisicion = convertirFechaCorta(n.fechaAdquisicion);
     if (n.titular) {
-      if (n.titular[0].clave != "DEC") {
-        console.log(n);
-        delete n;
-      }
-
-        if (n.titular[0].clave === "DEC") {
-          if (n.tercero[0].tipoPersona === null) {
+      if (n.titular[0].clave === "DEC") {
+        if (n.tercero[0].tipoPersona === null) {
+          delete n.tercero;
+        }
+        if (n.tercero) {
+          if (n.tercero[0].tipoPersona === "FISICA") {
             delete n.tercero;
           }
-          if (n.tercero) {
-            if (n.tercero[0].tipoPersona === "FISICA") {
-              delete n.tercero;
+          else {
+            if (n.tercero[0].nombreRazonSocial === null) {
+              n.tercero[0].nombreRazonSocial = "";
             }
-            else {
-              if (n.tercero[0].nombreRazonSocial === null) {
-                n.tercero[0].nombreRazonSocial = "";
-              }
-              if (n.tercero[0].rfc === null) {
-                n.tercero[0].rfc = "";
-              }
+            if (n.tercero[0].rfc === null) {
+              n.tercero[0].rfc = "";
             }
-          }
-          if (n.transmisor[0].tipoPersona === null) {
-            delete n.transmisor;
-          }
-          if (n.transmisor) {
-            if (n.transmisor[0].tipoPersona === "FISICA") {
-              delete n.transmisor;
-            }
-            else {
-              if (n.transmisor[0].nombreRazonSocial === null) {
-                n.transmisor[0].nombreRazonSocial = "";
-              }
-              if (n.transmisor[0].rfc === null) {
-                n.transmisor[0].rfc = "";
-              }
-            }
-          }
-          if (n.motivoBaja === null) {
-            delete n.motivoBaja;
-          }
-          if (n.formaPago === 'CREDITO') {
-            n.formaPago = 'CRÉDITO';
-          }
-          if (n.formaPago === 'NO_APLICA') {
-            n.formaPago = 'NO APLICA';
           }
         }
-      
+        if (n.transmisor[0].tipoPersona === null) {
+          delete n.transmisor;
+        }
+        if (n.transmisor) {
+          if (n.transmisor[0].tipoPersona === "FISICA") {
+            delete n.transmisor;
+          }
+          else {
+            if (n.transmisor[0].nombreRazonSocial === null) {
+              n.transmisor[0].nombreRazonSocial = "";
+            }
+            if (n.transmisor[0].rfc === null) {
+              n.transmisor[0].rfc = "";
+            }
+          }
+        }
+        if (n.motivoBaja === null) {
+          delete n.motivoBaja;
+        }
+        if (n.formaPago === 'CREDITO') {
+          n.formaPago = 'CRÉDITO';
+        }
+        if (n.formaPago === 'NO_APLICA') {
+          n.formaPago = 'NO APLICA';
+        }
+      }
+    }
+    else{
+      delete n;
     }
     return n;
   })
