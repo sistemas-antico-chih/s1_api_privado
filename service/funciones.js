cb5c9a1ae99c700c6032c3d54a5b79a174768dd8
= <=>
2022-10-21 08:34:43 -0600

    prueba ingresos persona null

--- service/funciones.js
+++ service/funciones.js
@@ -263,9 +263,9 @@ function bienesInmuebles(bienInmueble) {
                /*if (n.tercero[0].tipoPersona === "FISICA") {
                   delete n.tercero;
                 }*/
-                if (n.tercero[0].tipoPersona === "null"){
+                if (n.tercero[0].tipoPersona === null){
                   delete n.tercero;
-                  console.log ("llega");
+                  console.log ("llega a null");
                 }
                 else {
                   if (n.tercero[0].nombreRazonSocial) {
