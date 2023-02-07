7170761ca15de5a70342b5e45428d474017d7808
= <=>
2023-02-07 09:57:04 -0600

    ordenamiento bienesInmuebles

--- service/declaracionesService.js
+++ service/declaracionesService.js
@@ -295,20 +295,21 @@ async function post_declaraciones(body) {
       if (key === "bienesInmuebles") {
         if (value.superficieConstruccion) {
           //newSort[key + ".bienInmueble.superficieConstruccion.valor"] = value.superficieConstruccion
-          newSort = { 
-                      "bienesInmuebles.bienInmueble.superficieConstruccion.valor": value }
+          newSort = { "bienesInmuebles.bienInmueble.titular.0.titularDec": value.superficieConstruccion, 
+                      "bienesInmuebles.bienInmueble.superficieConstruccion.valor": value.superficieConstruccion }
         }
         if (value.superficieTerreno) {
           //newSort[key + ".bienInmueble.superficieTerreno.valor"] = value.superficieTerreno
-          newSort = { "bienesInmuebles.bienInmueble.titular.0.titularDec": value }
+          newSort = { "bienesInmuebles.bienInmueble.titular.0.titularDec": value.superficieTerreno,
+                      "bienesInmuebles.bienInmueble.superficieTerreno.valor": value.superficieTerreno }
         }
         if (value.formaAdquisicion) {
           newSort[key + ".bienInmueble.formaAdquisicion.clave"] = value.formaAdquisicion
         }
         if (value.valorAdquisicion) {
           //newSort[key + ".bienInmueble.valorAdquisicion.valor"] = value.valorAdquisicion
-          newSort = { "bienesInmuebles.bienInmueble.titular.0.titularDec": value, 
-                      "bienesInmuebles.bienInmueble.valorAdquisicion.valor": value }
+          newSort = { "bienesInmuebles.bienInmueble.titular.0.titularDec": value.valorAdquisicion, 
+                      "bienesInmuebles.bienInmueble.valorAdquisicion.valor": value.valorAdquisicion }
         }
       }
     }
